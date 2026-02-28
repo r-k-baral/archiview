def9fab378416664f08c54d38587f5209db06be9
r-k-baral <160538972+r-k-baral@users.noreply.github.com>
2026-02-28 19:46:30 +0530

    added a some portion of home pages

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,7 @@
 import Navbar from "components/Navbar";
 import type { Route } from "./+types/home";
+import { ArrowRight, ArrowUpFromLine, ArrowUpRight, Clock, Layers } from "lucide-react";
+import  Button  from "../../components/ui/Button"
 
 
 export function meta({}: Route.MetaArgs) {
@@ -11,9 +13,83 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return (
-  <div>
+  <div className="home">
     <Navbar/>
-  <h1 className="text-3xl text-indigo-700 font-extrabold">hello g</h1>
+  <section className="hero">
+    <div className="announce">
+      <div className="dot">
+        <div className="pulse"></div>
+
+      </div>
+    <p>Intrudecing archiview</p>      
+    </div>
+    <h1>Built that Moves at the Speed of Vision Beautiful Spaces. Intelligent Design. </h1>
+    <p className="subtitle">The Future of Architecture is IntelligentCreating Living Spaces Powered by AI Architecture Reimagined Through AI Innovation</p>
+
+    <div className="actions">
+    <a href="#upload" className="cta">
+      start building <ArrowRight className="icon" />
+    </a>
+    
+    <Button variant="outline" size="lg" className="demo">
+    Watch Demo
+    </Button>
+    </div>
+    <div id="upload" className="upload-shell">
+      <div className="grid-overlay"/>
+
+      <div className="upload-card">
+        <div className="upload-head">
+            <div className="upload-icon">
+              <ArrowUpFromLine className="icon" />
+
+            </div>
+            <h3>Upload your plan</h3>
+            <p>Support JGP, PNG,  formats up to 10MB </p>
+        </div>
+        <p>upload Images</p>
+      </div>
+    </div>
+  </section>
+
+  <section  className="projects">
+    <div className="section-inner">
+      <div className="section-head">
+        <div className="copy">
+          <h2>Project</h2>
+          <p>your latest work shared of pepole</p>
+        </div>
+
+      </div>
+      <div className="projects-grid">
+        <div className="project-card group">
+          <div className="preview">
+            <img src="https://wpmedia.roomsketcher.com/content/uploads/2021/12/24152947/cutecore-apartment-2d-and-3d-floor-plan.jpg" alt="project" />
+            <div className="badge">
+              <span>Community</span>
+            </div>
+
+          </div>
+        <div className="card-body">
+          <div>
+            <h3>Project Home</h3>
+            <div className="meta">
+              <Clock size={12}/>
+              <span>{new Date('01.15.2026').toLocaleDateString()}</span>
+              <span>By gopi</span>
+            </div>
+          </div>
+          <div className="arrow">
+            <ArrowUpRight size={18}/>
+          </div>
+        </div>
+        </div>
+      </div>
+
+
+    </div>
+
+  </section>
     </div>
   )
 }
